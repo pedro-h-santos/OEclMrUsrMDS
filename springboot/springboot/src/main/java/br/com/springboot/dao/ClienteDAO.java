8e9d11258d3ec7236032e51334c1daed60a1266a
pedro-h-santos <pedro.henriq.sants@gmail.com>
2022-10-07 10:32:58 -0300

    Cliente DAO & CRUD Add

--- springboot/springboot/src/main/java/br/com/springboot/dao/ClienteDAO.java
+++ springboot/springboot/src/main/java/br/com/springboot/dao/ClienteDAO.java
@@ -1,5 +1,39 @@
 package br.com.springboot.dao;
+import java.util.List;
 
-public class ClienteDAO {
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
+import org.springframework.stereotype.Repository;
+
+import br.com.springboot.model.Cliente;
+
+@Repository
+public class ClienteDAO implements CRUD<Cliente, Long> {
+    @PersistenceContext
+    private EntityManager entityManager;
+    @Override
+    public Cliente pesquisaPeloId(Long id){
+        return entityManager.find(Cliente.class, id);
+    }
+    @Override
+    public List<Cliente> lista() {
+        Query query = entityManager.createQuery("SELECT c FROM Cliente c");
+        return (List<Cliente>) query.getResultList();
+    }
+    @Override
+    public void insere(Cliente cliente){
+        entityManager.persist(cliente);
+    }
+    @Override
+    public void atualiza(Cliente cliente){
+        entityManager.merge(cliente);
+    }
+    @Override
+    public void remove(Cliente cliente){
+        entityManager.remove(cliente);
+    }
 }
+
+
